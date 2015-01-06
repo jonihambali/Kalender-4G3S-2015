ff0621c420973869eb182317ebc44801ea804119
abahgomel <abahgomel@gmail.com>
2015-01-06 09:27:32 +0700

    Create MyWebViewFragment.java

---
+++ MyWebViewFragment.java
@@ -0,0 +1,58 @@
+package com.spsigt.kalender4g3s2015;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyWebViewFragment extends Fragment {
+
+	ProgressDialog mProgress;
+	WebView webview;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.web_fragment, container,
+				false);
+
+		
+		Bundle bundle = getArguments();
+
+		String url = bundle.getString("url");
+
+		webview = (WebView) rootView.findViewById(R.id.webview1);
+
+		WebSettings settings = webview.getSettings();
+		settings.setJavaScriptEnabled(true);
+
+		mProgress = ProgressDialog.show(getActivity(), "Loading",
+				"Silahkan Tunggu Beberapa Saat...");
+		webview.loadUrl(url);
+
+		webview.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+				view.loadUrl(url);
+				return true;
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				if (mProgress.isShowing()) {
+					mProgress.dismiss();
+				}
+			}
+		});
+
+		return rootView;
+	}
+
+}
